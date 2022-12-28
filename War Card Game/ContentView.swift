55c34b85cb349a6ab47b831a0c33df1b53ee35d4
Chris Carey <crc2220@gmail.com>
2022-12-27 22:16:24 -0500

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Chris Carey on 12/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Chris Carey on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
